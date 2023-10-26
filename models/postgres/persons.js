27528c1f839dc6571b4a61f509a239eeeca75bfe
unknown <edwingc88@gmail.com>
2023-10-26 14:45:12 -0400

    Inyeccion de dependencias edit

--- models/postgres/persons.js
+++ models/postgres/persons.js
@@ -5,7 +5,7 @@ const { Pool } = pkg
 let conn
 
 // .ENV
-
+/*
 if (!conn) {
   conn = new Pool({
     user: process.env.DB_USER,
@@ -15,15 +15,15 @@ if (!conn) {
     database: process.env.DB_NAME
   })
 }
+*/
 
-/*
 if (!conn) {
   conn = new Pool({
     connectionString: process.env.DATABASE_URL
     // ssl: true
   })
 }
-*/
+
 
 export class PersonModel {
   static async getAll ({ role }) {
